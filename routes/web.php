b2dbdda8c63e93b596758ffd31eea83fe0e4a862
Wicaksu <wicaksu@wicaksu.com>
2024-01-11 23:10:18 +0700

    ok

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,6 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    // Route::get('/dashboard', function () {
-    //     return view('dashboard');
-    // })->name('dashboard');
     Route::get('/dashboard', function () {
         return view('user.test');
     });
