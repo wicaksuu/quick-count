4318d43f149569687fbe43243c7bb6bb3206121a
Wicaksu <wicaksu@wicaksu.com>
2024-02-11 05:43:24 +0700

    fixx

--- resources/views/admin/pilkada/grafik-presiden.blade.php
+++ resources/views/admin/pilkada/grafik-presiden.blade.php
@@ -1,5 +1,5 @@
 <div class="grid grid-cols-1 gap-5 xl:grid-cols-12">
-    <div class="col-span-4">
+    <div class="col-span-6">
         <div class="card dark:bg-zinc-800 dark:border-zinc-600">
             <div class="pb-0 card-body">
                 <h6 class="mb-1 text-gray-600 text-15 dark:text-gray-100">Perolehan Suara Presiden</h6>
@@ -9,12 +9,12 @@
             </div>
         </div>
     </div>
-    <div class="col-span-4">
+    <div class="col-span-6">
         <div class="card dark:bg-zinc-800 dark:border-zinc-600">
             <div class="pb-0 card-body">
                 <h6 class="mb-1 text-gray-600 text-15 dark:text-gray-100">Sebaran Suara Presiden</h6>
             </div>
-            <div class="flex flex-wrap gap-3 card-body" style="height: 275px;">
+            <div class="flex flex-wrap gap-3 card-body" style="height: 410px;">
                 <canvas id="PiePresiden"></canvas>
                 <div class="col-span-6 p-3">
                     <div class="mb-2 mt-sm-0">
@@ -41,7 +41,7 @@
                                 <i class="mdi mdi-circle align-middle text-10 mr-2 text-green-500"></i>
                                 <span id="Presiden3"></span>
                             </p>
-                            <h6 class="text-gray-800 dark:text-gray-100  pl-6">
+                            <h6 class="text-gray-800 dark:text-gray-100 pl-6">
                                 <span id="SuaraPresiden3"></span>
                             </h6>
                         </div>
