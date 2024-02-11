53050c8510648dc1d0cf6949bc976e569a2971cb
Wicaksu <wicaksu@wicaksu.com>
2024-02-11 13:17:21 +0700

    dsa

--- resources/views/livewire/componen/admin/navigasi/d-p-d-r-i.blade.php
+++ resources/views/livewire/componen/admin/navigasi/d-p-d-r-i.blade.php
@@ -9,7 +9,7 @@
         </a>
         <ul>
             <li>
-                <a href="{{ route('dashboard-over-view',['id'=>'dpr-ri']) }}" class="pl-14 pr-4 py-2 block text-[13.5px] font-medium text-gray-700 transition-all duration-150 ease-linear hover:text-violet-500 dark:text-gray-300 dark:active:text-white dark:hover:text-white">
+                <a href="{{ route('dashboard-over-view',['id'=>'dpd-ri']) }}" class="pl-14 pr-4 py-2 block text-[13.5px] font-medium text-gray-700 transition-all duration-150 ease-linear hover:text-violet-500 dark:text-gray-300 dark:active:text-white dark:hover:text-white">
                     Dashboard
                 </a>
             </li>
