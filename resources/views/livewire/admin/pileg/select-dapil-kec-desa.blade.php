9723bf5f1db550ac8704b715712e376219d778c3
Wicaksu <wicaksu@wicaksu.com>
2024-02-11 11:48:04 +0700

    fix hot

--- resources/views/livewire/admin/pileg/select-dapil-kec-desa.blade.php
+++ resources/views/livewire/admin/pileg/select-dapil-kec-desa.blade.php
@@ -32,7 +32,7 @@
         <div>
             <select wire:model="pilihtps" wire:click='GetDataDesa'
                 class="dark:bg-zinc-800 dark:border-zinc-700 w-full rounded border-gray-100 py-2.5 text-sm text-gray-500 focus:border focus:border-violet-500 focus:ring-0 dark:bg-zinc-700/50 dark:text-zinc-100">
-                <option value="">Pilih Desa</option>
+                <option value="">Pilih Tps</option>
                     @foreach ($tpss as $tps)
                         <option value="{{ $tps->id }}">{{ $tps->nama }}</option>
                     @endforeach
